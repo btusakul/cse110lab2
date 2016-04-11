325a546f5a00f79545255e153bd88a451b7f1216
Bryan Tusakul <btusakul@ucsd.edu>
2016-04-11 12:22:24 -0700

    new_commit

--- app/src/main/java/com/example/btusa_000/cse110lab2/MyActivity.java
+++ app/src/main/java/com/example/btusa_000/cse110lab2/MyActivity.java
@@ -27,7 +27,7 @@ public class MyActivity extends AppCompatActivity {
             }
         });
     }
-
+    //I changed the text to yellow!!!
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
